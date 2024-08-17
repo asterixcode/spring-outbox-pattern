2020e56fb4347e1496e04ce81d7d8071665c11d7
lucas.saldanha <lk.saldanha@gmail.com>
2024-08-17 17:45:21 +0200

    add outbox record

---
+++ order-service/src/main/java/com/asterixcode/order_service/outbox/Outbox.java
@@ -0,0 +1,28 @@
+package com.asterixcode.order_service.outbox;
+
+import com.asterixcode.order_service.orders.Order;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.Date;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Column;
+import org.springframework.data.relational.core.mapping.Table;
+
+@Table("outbox")
+public record Outbox(
+    @Id Integer id,
+    @Column("aggregate_id") String aggregateId,
+    String payload,
+    @Column("created_at") Date createdAt,
+    boolean processed) {
+
+  // constructor that maps from Order to Outbox
+  public Outbox(Order order) throws JsonProcessingException {
+    this(
+        null,
+        order.id().toString(),
+        new ObjectMapper().writeValueAsString(order),
+        new Date(),
+        false);
+  }
+}
