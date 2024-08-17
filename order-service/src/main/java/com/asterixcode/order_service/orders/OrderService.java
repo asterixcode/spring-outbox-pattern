0cab583bce4dd067840753c3db3375aa8e32bfbf
lucas.saldanha <lk.saldanha@gmail.com>
2024-08-17 18:20:22 +0200

    feat: add loggers

--- order-service/src/main/java/com/asterixcode/order_service/orders/OrderService.java
+++ order-service/src/main/java/com/asterixcode/order_service/orders/OrderService.java
@@ -1,14 +1,18 @@
 package com.asterixcode.order_service.orders;
 
-import com.asterixcode.order_service.outbox.Outbox;
 import com.asterixcode.order_service.exceptions.OutboxException;
+import com.asterixcode.order_service.outbox.Outbox;
 import com.asterixcode.order_service.outbox.OutboxRepository;
 import com.fasterxml.jackson.core.JsonProcessingException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class OrderService {
+
+  private static final Logger logger = LoggerFactory.getLogger(OrderService.class);
 
   private final OrderRepository orderRepository;
   private final OutboxRepository outboxRepository;
@@ -25,9 +29,11 @@ public class OrderService {
     try {
       Outbox outbox = new Outbox(order);
       outboxRepository.save(outbox);
+      logger.info("Order saved in the outbox: {}", outbox);
     } catch (JsonProcessingException e) {
       throw new OutboxException("Error saving order in the outbox", e);
     }
+    logger.info("Order created: {}", order);
     return order;
   }
 }
