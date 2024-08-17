0a70425f2454ec949a23233833b6819c07ff4291
lucas.saldanha <lk.saldanha@gmail.com>
2024-08-17 17:47:13 +0200

    feat: start order-puller module

---
+++ order-puller/src/test/java/com/asterixcode/order_puller/OrderPullerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.asterixcode.order_puller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderPullerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
